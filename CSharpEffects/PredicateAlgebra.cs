879b211f735e6ffdc59f735012c408c8e11ca1e6
Radu Popovici <rpopovici@totalsoft.ro>
2021-02-03 06:29:05 +0200

    predicate algebra

---
+++ CSharpEffects/PredicateAlgebra.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEffects
+{
+    public static class PredicateAlgebra
+    {
+        static Predicate<int> isEven = x => x % 2 == 0;
+
+        public static Predicate<B> Map<A, B>(Func<A, B> fn, Predicate<A> source) => //does not work
+            x => false;
+    }
+}

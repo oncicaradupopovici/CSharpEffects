e7e15292d6d191d0cb9bec20ccee181d761e2b5d
Radu Popovici <rpopovici@totalsoft.ro>
2021-02-03 10:21:21 +0200

    Functor algebra tentative

---
+++ CSharpEffects/FunctorAlgebra.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEffects
+{
+    //interface Functor<F>
+    //{
+    //    F<B> Map<A, B>(Func<A, B> fn, F<A> source);
+    //}
+
+    //class LazyFunctor : Functor<Lazy>
+    //{
+    //    public Lazy<B> Map<A, B>(Func<A, B> fn, Lazy<A> source) 
+    //        => LazyAlgebra.Map(fn, source);
+    //}
+}
